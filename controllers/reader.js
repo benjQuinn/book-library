b092267cca47cde90ef049167c7549dd63b3d028
Ben Quinn <benjamesquinn@gmail.com>
2022-05-17 22:16:11 +0100

    Passed get reader by id tests

--- controllers/reader.js
+++ controllers/reader.js
@@ -1,11 +1,32 @@
 const { Reader } = require("../src/models");
 
 exports.create_reader = async (req, res) => {
-  const newReader = await Reader.create(req.body);
-  res.status(201).json(newReader);
+  try {
+    const newReader = await Reader.create(req.body);
+    res.status(201).json(newReader);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 exports.get_readers = async (req, res) => {
-  const readers = await Reader.findAll();
-  res.status(200).json(readers);
+  try {
+    const readers = await Reader.findAll();
+    res.status(200).json(readers);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.get_reader_id = async (req, res) => {
+  const id = req.params.readerId;
+  const reader = await Reader.findByPk(id);
+
+  try {
+    !reader
+      ? res.status(404).json({ error: "The reader could not be found." })
+      : res.status(200).json(reader);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
--- routes/reader.js
+++ routes/reader.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 // GET
 router.get("/", readerController.get_readers);
+router.get("/:readerId", readerController.get_reader_id);
 
 // POST
 router.post("/", readerController.create_reader);
